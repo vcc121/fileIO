b4999f54441bbd1ec8f9f2396c9c03f5ce9bfe41
vcc121 <vaughn.cox@bsu.edu>
2025-02-14 15:40:02 +0000

    started file io cpp file

---
+++ fileio.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main(){
+	std::ifstream inFile;
+	std::stringstream converter;
+	std::string currentLine;
+
+	std::string sNum1;
+	std::string sNum2;
+	std::string text;
+
+	int num1;
+	int num2;
+	int sum;
+
+	inFile.open("data.csv");
+	while(getline(inFile, currentLine)){
+		converter.clear();
+		converter.str(currentLine);
+
+		getLine(converter, sNum1, ',');
+		getLine(converter, sNum2, ',');
+		getLine(converter, text);
+		
+		converter.clear();
+		converter.str("");
+		converter << sNum1 << " " << sNum2;
+		converter >> num1 >> num2;
+
+
